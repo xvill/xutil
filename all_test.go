5d2cd33e60495c76a951b3d7906244fb4783c0d6
xvill <xingg0088@126.com>
2018-05-15 11:04:17 +0800

    Update all_test.go

--- all_test.go
+++ all_test.go
@@ -9,6 +9,6 @@ import (
 
 func Test_Wgs2bd(t *testing.T) {
 	lat, lon := 31.2355502882, 121.5012091398
-	fmt.Println(xtools.Wgs2bd(lat, lon))
+	fmt.Println(xutil.Wgs2bd(lat, lon))
 	// 121.486245,31.3838164	121.47521,31.37982
 }
