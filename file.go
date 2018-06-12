b1bae5224cd87f7c7b72d42ea8dc4369fc1e4879
xvill <xingg0088@126.com>
2018-06-12 14:33:06 +0800

    Update file.go

--- file.go
+++ file.go
@@ -5,6 +5,19 @@ import (
 	"os"
 )
 
+//IsFileExist 文件是否存在
+func IsFileExist(path string) (isExist, isDir bool, err error) {
+	fi, err := os.Stat(path)
+	if err == nil {
+		return true, fi.IsDir(), nil
+	}
+	if os.IsNotExist(err) {
+		return false, false, errors.New("no such file or dir")
+	}
+	return false, false, err
+}
+
+
 // CsvWriteALL 生成CSV
 func CsvWriteALL(data [][]string, wfile string, comma rune) error {
 	wf, err := os.Create(wfile)
