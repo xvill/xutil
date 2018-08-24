4bc82a7a7aa76323260efb166a3f378e61536036
xvill <xingg0088@126.com>
2018-08-24 14:58:48 +0800

    Update net.go

--- net.go
+++ net.go
@@ -2,38 +2,21 @@ package xutil
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
+	"net"
 )
 
-func Ip2long(ipstr string) (ip uint32) {
-	r := `^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})`
-	reg, err := regexp.Compile(r)
-	if err != nil {
-		return
-	}
-	ips := reg.FindStringSubmatch(ipstr)
-	if ips == nil {
-		return
-	}
-
-	ip1, _ := strconv.Atoi(ips[1])
-	ip2, _ := strconv.Atoi(ips[2])
-	ip3, _ := strconv.Atoi(ips[3])
-	ip4, _ := strconv.Atoi(ips[4])
-
-	if ip1 > 255 || ip2 > 255 || ip3 > 255 || ip4 > 255 {
-		return
-	}
-
-	ip += uint32(ip1 * 0x1000000)
-	ip += uint32(ip2 * 0x10000)
-	ip += uint32(ip3 * 0x100)
-	ip += uint32(ip4)
-
-	return
+func Ip2long(ip net.IP) uint32 {
+	a := uint32(ip[12])
+	b := uint32(ip[13])
+	c := uint32(ip[14])
+	d := uint32(ip[15])
+	return uint32(a<<24 | b<<16 | c<<8 | d)
 }
 
-func Long2ip(ip uint32) string {
-	return fmt.Sprintf("%d.%d.%d.%d", ip>>24, ip<<8>>24, ip<<16>>24, ip<<24>>24)
+func Long2ip(ip uint32) net.IP {
+	a := byte((ip >> 24) & 0xFF)
+	b := byte((ip >> 16) & 0xFF)
+	c := byte((ip >> 8) & 0xFF)
+	d := byte(ip & 0xFF)
+	return net.IPv4(a, b, c, d)
 }
