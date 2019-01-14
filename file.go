691393c25cc82415284c571dc577d2955c5d1186
xvill <xingg0088@126.com>
2019-01-14 16:51:51 +0800

    Update file.go

--- file.go
+++ file.go
@@ -3,7 +3,12 @@ package xutil
 import (
 	"encoding/csv"
 	"errors"
+	"io"
 	"os"
+	"time"
+
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
 )
 
 //IsFileExist 文件是否存在
@@ -18,7 +23,6 @@ func IsFileExist(path string) (isExist, isDir bool, err error) {
 	return false, false, err
 }
 
-
 // CsvWriteALL 生成CSV
 func CsvWriteALL(data [][]string, wfile string, comma rune) error {
 	wf, err := os.Create(wfile)
@@ -32,4 +36,76 @@ func CsvWriteALL(data [][]string, wfile string, comma rune) error {
 	wcsv.Flush()
 
 	return nil
+}
+
+type SSftp struct {
+	Ssh  *ssh.Client
+	Sftp *sftp.Client
+}
+
+func NewSSftp(user, passwd, hostport string) (s SSftp, err error) {
+	config := ssh.ClientConfig{
+		User:            user,
+		Auth:            []ssh.AuthMethod{ssh.Password(passwd)},
+		Timeout:         30 * time.Second,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey()}
+	conn, err := ssh.Dial("tcp", hostport, &config)
+	if err != nil {
+		return s, err
+	}
+	c, err := sftp.NewClient(conn)
+	if err != nil {
+		conn.Close()
+		return s, err
+	}
+	s.Ssh = conn
+	s.Sftp = c
+	return s, nil
+}
+
+func (s SSftp) Put(scpfiles [][2]string) error {
+	for _, v := range scpfiles {
+		srcFile, err := os.Open(v[0])
+		if err != nil {
+			return err
+		}
+		dstFile, err := s.Sftp.Create(v[1])
+		if err != nil {
+			return err
+		}
+		buf := make([]byte, 1024)
+		_, err = io.CopyBuffer(dstFile, srcFile, buf)
+		srcFile.Close()
+		dstFile.Close()
+	}
+	return nil
+}
+
+func (s SSftp) Get(scpfiles [][2]string) error {
+	for _, v := range scpfiles {
+		srcFile, err := s.Sftp.Open(v[0])
+		if err != nil {
+			return err
+		}
+		dstFile, err := os.Create(v[1])
+		if err != nil {
+			return err
+		}
+		_, err = srcFile.WriteTo(dstFile)
+		if err != nil {
+			return err
+		}
+		srcFile.Close()
+		dstFile.Close()
+	}
+	return nil
+}
+
+func (s SSftp) Close() error {
+	err := s.Sftp.Close()
+	if err != nil {
+		return err
+	}
+	err = s.Ssh.Close()
+	return err
 }
