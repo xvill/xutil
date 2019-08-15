e4b739111a2ccafac7ed221fb682cb9478632a57
xvill <xingg0088@126.com>
2019-08-15 16:27:55 +0800

    Update gis.go

--- gis.go
+++ gis.go
@@ -7,10 +7,11 @@ import (
 
 // ToFixed 浮点数保留
 func ToFixed(f float64, n int) float64 {
-	shift := math.Pow(10, float64(n))
+	shift := math.Pow10(n)
 	fv := 0.0000000001 + f //对浮点数产生.xxx999999999 计算不准进行处理
 	return math.Floor(fv*shift+.5) / shift
 }
+
 
 //===============================================================================
 /*
