ccfc65426ec602cdb50fd24765d6fc07be89b28d
xvill <xingg0088@126.com>
2019-12-09 15:10:03 +0800

    Update ftp.go

--- ftp.go
+++ ftp.go
@@ -6,86 +6,96 @@ import (
 	"path/filepath"
 	"time"
 
-	ftp "github.com/jlaffaye/ftp"
+	ftp4go "github.com/shenshouer/ftp4go"
 )
 
-type Ftp struct {
-	Host        string
-	User        string
-	Pwd         string
-	Conn        *ftp.ServerConn
-	DisableEPSV bool
-	Timeout     time.Duration
+type XFtp struct {
+	Addr            string
+	User            string
+	Pwd             string
+	PASV            string
+	FilePattern     string
+	LocalFilePrefix string
+	Conn            *ftp4go.FTP
 }
 
-func (c *Ftp) OpenFTP() (err error) {
-	if c.Timeout == 0 {
-		c.Timeout = 10*time.Second
-	}
-	c.Conn, err = ftp.Dial(c.Host, ftp.DialWithDisabledEPSV(c.DisableEPSV), ftp.DialWithTimeout(c.Timeout))
+func (c *XFtp) Connect() (err error) {
+	addra := strings.Split(c.Addr, ":")
+	host := addra[0]
+	port, _ := strconv.Atoi(addra[1])
+
+	c.Conn = ftp4go.NewFTP(0) // 1 for debugging
+	_, err = c.Conn.Connect(host, port, "")
 	if err != nil {
 		return err
 	}
-	err = c.Conn.Login(c.User, c.Pwd)
+
+	_, err = c.Conn.Login(c.User, c.Pwd, "")
 	if err != nil {
 		return err
+	}
+	if c.PASV == "PORT" {
+		c.Conn.SetPassive(false)
 	}
 	return nil
 }
 
-func (c Ftp) NameList(pattern string) (ftpfiles []string) {
-	files, err := c.Conn.NameList(filepath.Dir(pattern))
+func (c XFtp) NameList() (ftpfiles []string) {
+	files, err := c.Conn.Nlst(filepath.Dir(c.FilePattern))
 	if err != nil {
 		fmt.Println(err)
 		return nil
 	}
 
 	for _, v := range files {
-		if ok, _ := filepath.Match(pattern, v); ok {
+		if ok, _ := filepath.Match(c.FilePattern, v); ok {
 			ftpfiles = append(ftpfiles, v)
 		}
 	}
 	return ftpfiles
 }
 
-func (c Ftp) FilesByPattern(pattern []string) (dat map[string][]byte, err error) {
-	files := make([]string, 0)
-	for _, par := range pattern {
-		files = append(files, c.NameList(par)...)
-	}
-	return c.Files(files)
-}
 
-func (c Ftp) Files(files []string) (dat map[string][]byte, err error) {
-	dat = make(map[string][]byte, 0)
-	for _, file := range files {
-		r, err := c.Conn.Retr(file)
-		if err != nil {
-			return dat, err
+func (c XFtp) DownloadFiles() (dat map[string]string, err error) {
+	files:=c.NameList()
+	dat = make(map[string]string, 0)
+	if c.LocalFilePrefix != ""{
+		x,dir,_:= IsFileExist(c.LocalFilePrefix)
+		if x&&dir{
+			c.LocalFilePrefix = filepath.Dir(c.LocalFilePrefix +string(filepath.Separator)) + string(filepath.Separator)
 		}
-		buf, err := ioutil.ReadAll(r)
-		if err != nil {
-			return dat, err
-		}
-		dat[file] = buf
-		r.Close()
 	}
+	fmt.Println("DownloadFiles begin")
+	for _, file := range files {
+		if c.LocalFilePrefix == "" {
+			c.LocalFilePrefix = time.Now().Format("20060102150405") + "_"
+		}
+		localpath := c.LocalFilePrefix + filepath.Base(file)
+		fmt.Println("DownloadFile "+file+" to "+localpath)
+		err = c.Conn.DownloadFile(file, localpath, false)
+		if err != nil {
+			return dat, err
+		}
+		dat[file] = localpath
+	}
+	fmt.Println("DownloadFiles end")
 	return dat, nil
 }
 
-func (c Ftp) FileFTP(pattern []string) (files map[string][]byte, err error) {
-	err = c.OpenFTP()
+func (c XFtp) Logout() error {
+	_, err := c.Conn.Quit()
+	return err
+}
+
+func (c *XFtp) ConnectAndDownload() (files map[string]string, err error) {
+	err = c.Connect()
 	if err != nil {
 		return nil, err
 	}
-	files, err = c.FilesByPattern(pattern)
-
+	defer c.Logout()
+	files, err = c.DownloadFiles()
 	if err != nil {
 		return nil, err
 	}
 	return files, nil
-}
-
-func (c Ftp) Logout() error {
-	return c.Conn.Logout()
 }
