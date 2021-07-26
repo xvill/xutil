f15c23506610754ad87feb8058e96828d235ea65
xvill <xingg0088@126.com>
2021-07-26 14:15:19 +0800

    Create math.go

--- math.go
+++ math.go
@@ -13,27 +13,32 @@ func Abs(n int64) int64 {
 	return (n ^ y) - y
 }
 
-// Sum 和
-func Sum(arr []float64) (sum float64) {
-	for _, v := range arr {
-		sum = sum + v
+// ---------------------------------------------------------------------------------------
+
+func ColFloat64(raw, fields, oper string) (ret float64) {
+	dat := StringToFloat64(raw, fields)
+	if len(dat) == 0 {
+		return
 	}
-	return
+	switch oper {
+	case "SUM":
+		ret = SumFloat64(dat)
+	case "MAX":
+		ret = MaxFloat64(dat)
+	case "MIN":
+		ret = MinFloat64(dat)
+	case "AVG", "MEAN":
+		ret = AvgFloat64(dat)
+	case "STDDEV":
+		ret = StdDevFloat64(dat)
+	}
+
+	return ret
 }
 
-// Mean 均值
-func Mean(arr []float64) float64 {
-	var N, sum float64
-	for _, v := range arr {
-		N++
-		sum = sum + v
-	}
-	return sum / N
-}
-
-func DiffSqrtMean(a []float64) []float64 {
+func DiffSqrtMeanFloat64(a []float64) []float64 {
 	set := make([]float64, len(a))
-	meanVal := Mean(a)
+	meanVal := AvgFloat64(a)
 	var d float64
 	for i, v := range a {
 		d = v - meanVal
@@ -42,9 +47,77 @@ func DiffSqrtMean(a []float64) []float64 {
 	return set
 }
 
-func StdDev(a []float64) float64 {
-	return math.Sqrt(Mean(DiffSqrtMean(a)))
+func StdDevFloat64(a []float64) float64 {
+	return math.Sqrt(AvgFloat64(DiffSqrtMeanFloat64(a)))
 }
+
+func StringToFloat64(raw, fields string) (dat []float64) {
+	arr := strings.Split(raw, fields)
+	for i := 0; i < len(arr); i++ {
+		f, err := strconv.ParseFloat(arr[i], 64)
+		if err != nil {
+			// fmt.Println(raw, err.Error())
+			continue
+		}
+		dat = append(dat, f)
+	}
+	return
+}
+
+func SumFloat64(s []float64) (sum float64) {
+	for _, v := range s {
+		sum += v
+	}
+	return
+}
+
+func MaxFloat64(i []float64) float64 {
+	if len(i) == 0 {
+		panic("arg is an empty array/slice")
+	}
+	var max float64
+	for idx := 0; idx < len(i); idx++ {
+		item := i[idx]
+		if idx == 0 {
+			max = item
+			continue
+		}
+		if item > max {
+			max = item
+		}
+	}
+	return max
+}
+
+func MinFloat64(i []float64) float64 {
+	if len(i) == 0 {
+		panic("arg is an empty array/slice")
+	}
+	var min float64
+	for idx := 0; idx < len(i); idx++ {
+		item := i[idx]
+		if idx == 0 {
+			min = item
+			continue
+		}
+		if item < min {
+			min = item
+		}
+	}
+	return min
+}
+
+func MeanFloat64(i []float64) float64 {
+	return AvgFloat64(i)
+}
+
+func AvgFloat64(i []float64) float64 {
+	if len(i) == 0 {
+		panic("arg is an empty array/slice")
+	}
+	return SumFloat64(i) / float64(len(i))
+}
+
 //=====================任意进制转换=========================================
 var tenToAny map[int]string = map[int]string{0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "a", 11: "b", 12: "c", 13: "d", 14: "e", 15: "f", 16: "g", 17: "h", 18: "i", 19: "j", 20: "k", 21: "l", 22: "m", 23: "n", 24: "o", 25: "p", 26: "q", 27: "r", 28: "s", 29: "t", 30: "u", 31: "v", 32: "w", 33: "x", 34: "y", 35: "z", 36: ":", 37: ";", 38: "<", 39: "=", 40: ">", 41: "?", 42: "@", 43: "[", 44: "]", 45: "^", 46: "_", 47: "{", 48: "|", 49: "}", 50: "A", 51: "B", 52: "C", 53: "D", 54: "E", 55: "F", 56: "G", 57: "H", 58: "I", 59: "J", 60: "K", 61: "L", 62: "M", 63: "N", 64: "O", 65: "P", 66: "Q", 67: "R", 68: "S", 69: "T", 70: "U", 71: "V", 72: "W", 73: "X", 74: "Y", 75: "Z"}
 
@@ -98,4 +171,5 @@ func demoTo() {
 	fmt.Println(DecimalToAny(9999, 76))
 	fmt.Println(AnyToDecimal("1F[", 76))
 }
+
 //=======================================================================
